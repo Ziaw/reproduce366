f31c48f2745d48188a9324bb6f60d3d7449786ac
Yasuo Honda <yasuo.honda@gmail.com>
2013-11-11 23:07:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reproeduce366::Application.config.secret_key_base = '4203b1cbd4650b292ddc69be653b0c19a8dd2bbd73e7988797fe22ff4fea0696809914c1d40024a195dd52ce151ee48532ab6981d869e40dbd3906151d66ca74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reproeduce366::Application.config.session_store :cookie_store, key: '_reproeduce366_session'
